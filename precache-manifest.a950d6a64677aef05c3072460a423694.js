015b4c65fe2d02239f81f420d4d31e8a5d474fce
tpartridge1 <tammy.partridge7@gmail.com>
2020-02-17 10:26:56 -0500

    Updates

---
+++ precache-manifest.a950d6a64677aef05c3072460a423694.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00d1e59bd2fab1636492cbe822c2e43",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d7b98daf92178a566c77",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6a25d80b75a64b49acc8",
+    "url": "/robofriends/static/css/main.91b9d15f.chunk.css"
+  },
+  {
+    "revision": "d7b98daf92178a566c77",
+    "url": "/robofriends/static/js/2.a5962fcb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.a5962fcb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a25d80b75a64b49acc8",
+    "url": "/robofriends/static/js/main.a10d8c7a.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  },
+  {
+    "revision": "9b390e7028789833a2448e3a25581704",
+    "url": "/robofriends/static/media/ErrorBoundry.9b390e70.bin"
+  }
+]);
